22fe51ab3bacce96a5f78c590da139c5f7b126fb
Seunghun Lee <shiin.lee@samsung.com>
2016-11-28 17:51:17 +0900

    e_desk: Add a implementation of e_desk_zoom_set/unset.
    Newly Added APIs:
    E_API void         e_desk_zoom_set(E_Desk *desk, double zoomx, double zoomy, int cx, int cy);
    E_API void         e_desk_zoom_unset(E_Desk *desk);
    E_API void         e_desk_smart_member_add(E_Desk *desk, Evas_Object *obj);
    E_API void         e_desk_smart_member_del(Evas_Object *obj);
    
    - Test Plan:
      $ enlightenment_info -desk zoom [X's ZOOM RATIO] [Y's ZOOM RATIO] [CENTER X] [CENTER Y]
    
    - NOTE:
      You can unset zoom state by passing values 1.0 as a X and Y zoom ratio.
    
    Change-Id: I06860a5d1cfbc2e3fc43460cd428ea2bd894caeb

--- src/bin/e_info_server.c
+++ src/bin/e_info_server.c
@@ -2322,6 +2322,32 @@ _e_info_server_cb_desktop_geometry_set(const Eldbus_Service_Interface *iface EIN
    return reply;
 }
 
+static Eldbus_Message *
+_e_info_server_cb_desk_zoom(const Eldbus_Service_Interface *iface EINA_UNUSED, const Eldbus_Message *msg)
+{
+   Eldbus_Message *reply = eldbus_message_method_return_new(msg);
+   E_Zone *zone;
+   E_Desk *desk;
+   double zx, zy;
+   int cx, cy;
+
+   if (!eldbus_message_arguments_get(msg, "ddii", &zx, &zy, &cx, &cy))
+     {
+        ERR("Error getting arguments.");
+        return reply;
+     }
+
+   zone = e_zone_current_get();
+   desk = e_desk_current_get(zone);
+
+   if ((zx != 1.0) || (zy != 1.0))
+     e_desk_zoom_set(desk, zx, zy, cx, cy);
+   else
+     e_desk_zoom_unset(desk);
+
+   return reply;
+}
+
 static Eina_Bool
 _e_info_server_cb_buffer_change(void *data, int type, void *event)
 {
@@ -2797,6 +2823,7 @@ static const Eldbus_Method methods[] = {
    { "scrsaver", ELDBUS_ARGS({SIGNATURE_SCRSAVER_CLIENT, "scrsaver_params"}), ELDBUS_ARGS({SIGNATURE_SCRSAVER_SERVER, "scrsaver_result"}), _e_info_server_cb_scrsaver, 0},
    { "slot_message", ELDBUS_ARGS({"iiiiii", "slot_message"}), ELDBUS_ARGS({"a(ss)", "array of ec"}), e_info_server_cb_slot_message, 0},
    { "desktop_geometry_set", ELDBUS_ARGS({"iiii", "Geometry"}), NULL, _e_info_server_cb_desktop_geometry_set, 0},
+   { "desk_zoom", ELDBUS_ARGS({"ddii", "Zoom"}), NULL, _e_info_server_cb_desk_zoom, 0},
    { NULL, NULL, NULL, NULL, 0 }
 };
 
