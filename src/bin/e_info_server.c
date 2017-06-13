09f58a2df6327c051a093fb02dbc6f3cce2aff46
Konstantin Drabeniuk <k.drabeniuk@samsung.com>
2017-06-13 11:31:45 +0300

    e_info: add the scale factor option for enlightenment_info -dump_buffers
    Change-Id: I2092dca095b1a2652de9775c7b3bf36a6754cd66
    Signed-off-by: Konstantin Drabeniuk <k.drabeniuk@samsung.com>

--- src/bin/e_info_server.c
+++ src/bin/e_info_server.c
@@ -3661,8 +3661,9 @@ _e_info_server_cb_buffer_dump(const Eldbus_Service_Interface *iface EINA_UNUSED,
    int start = 0;
    int count = 0;
    const char *path = NULL;
+   double scale;
 
-   if (!eldbus_message_arguments_get(msg, "iis", &start, &count, &path))
+   if (!eldbus_message_arguments_get(msg, "iisd", &start, &count, &path, &scale))
      {
         ERR("Error getting arguments.");
         return reply;
@@ -3684,7 +3685,13 @@ _e_info_server_cb_buffer_dump(const Eldbus_Service_Interface *iface EINA_UNUSED,
         else
           {
              /* start dump */
-             tbm_surface_internal_dump_start(e_info_dump_path, e_comp->w, e_comp->h, count);
+             if (scale > 0.0)
+               tbm_surface_internal_dump_with_scale_start(e_info_dump_path,
+                                                          e_comp->w,
+                                                          e_comp->h,
+                                                          count, scale);
+             else
+                tbm_surface_internal_dump_start(e_info_dump_path, e_comp->w, e_comp->h, count);
              tdm_helper_dump_start(e_info_dump_path, &e_info_dump_count);
              E_LIST_HANDLER_APPEND(e_info_dump_hdlrs, E_EVENT_CLIENT_BUFFER_CHANGE,
                                _e_info_server_cb_buffer_change, NULL);
@@ -4862,7 +4869,7 @@ static const Eldbus_Method methods[] = {
    { "get_fps_info", NULL, ELDBUS_ARGS({"s", "fps request"}), _e_info_server_cb_fps_info_get, 0},
    { "punch", ELDBUS_ARGS({"iiiiiiiii", "punch_geometry"}), NULL, _e_info_server_cb_punch, 0},
    { "transform_message", ELDBUS_ARGS({"siiiiiiii", "transform_message"}), NULL, e_info_server_cb_transform_message, 0},
-   { "dump_buffers", ELDBUS_ARGS({"iis", "start"}), NULL, _e_info_server_cb_buffer_dump, 0 },
+   { "dump_buffers", ELDBUS_ARGS({"iisd", "start"}), NULL, _e_info_server_cb_buffer_dump, 0 },
    { "dump_selected_buffers", ELDBUS_ARGS({"ss", "dump_selected_buffers"}), NULL, _e_info_server_cb_selected_buffer_dump, 0 },
    { "output_mode", ELDBUS_ARGS({SIGNATURE_OUTPUT_MODE_CLIENT, "output mode"}), ELDBUS_ARGS({"a("SIGNATURE_OUTPUT_MODE_SERVER")", "array of ec"}), _e_info_server_cb_output_mode, 0 },
 #ifdef ENABLE_HWC_MULTI
