d10ed3f1db39e4e46c7e354046d6732f0d97808f
Junkyeong Kim <jk0430.kim@samsung.com>
2017-08-31 09:55:42 +0900

    e_comp_wl_screenshooter: erase e_comp_wl_screenshooter_dump function
    Change-Id: Id4b7c3aed942e52591081c310ed800e631f98a26
    Signed-off-by: Junkyeong Kim <jk0430.kim@samsung.com>

--- src/bin/e_comp_wl_screenshooter.h
+++ src/bin/e_comp_wl_screenshooter.h
@@ -6,7 +6,6 @@
 
 EINTERN int e_comp_wl_screenshooter_init(void);
 EINTERN void e_comp_wl_screenshooter_shutdown(void);
-EINTERN Eina_Bool e_comp_wl_screenshooter_dump(tbm_surface_h tbm_surface);
 
 #endif
 #endif
