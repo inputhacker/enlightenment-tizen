17a3ce92ea4032648244f9dea038a68ff355f797
Sung-Jin Park <sj76.park@samsung.com>
2017-09-19 14:43:09 +0900

    e_mouse: fix to use e_input_devices_get() instead of ecore_drm API
    Change-Id: I8a526dafc5da1e5f4bff9691787634527189eb88
    Signed-off-by: Sung-Jin Park <sj76.park@samsung.com>

--- src/bin/e_mouse.c
+++ src/bin/e_mouse.c
@@ -6,7 +6,7 @@ e_mouse_update(void)
    const Eina_List *list, *l;
    E_Input_Device *dev;
 
-   list = ecore_drm_devices_get();
+   list = e_input_devices_get();
    EINA_LIST_FOREACH(list, l, dev)
      {
         e_input_device_pointer_left_handed_set(dev, (Eina_Bool)!e_config->mouse_hand);
