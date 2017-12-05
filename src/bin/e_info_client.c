b97e6c947fade9f9cd91608b57aa7f4ca5a7e653
Doyoun Kang <doyoun.kang@samsung.com>
2017-12-05 10:54:30 +0900

    e_info_client: fixed buffer size warning
    Change-Id: I0b656b19f208d672c5c7ca5b142c7383e51cb203

--- src/bin/e_info_client.c
+++ src/bin/e_info_client.c
@@ -2404,8 +2404,10 @@ _e_info_client_proc_buffer_shot(int argc, char **argv)
                  }
 
                str_len = strlen(tmp_path);
+               if (str_len >= PATH_MAX) str_len = PATH_MAX - 1;
 
-               strncpy(path, tmp_path, PATH_MAX < str_len ? PATH_MAX : str_len);
+               strncpy(path, tmp_path, str_len);
+               path[str_len] = 0;
 
                free(tmp_path);
                continue;
