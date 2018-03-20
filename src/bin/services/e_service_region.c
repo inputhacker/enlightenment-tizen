bf8b87bb159c34785e6eb33e4274dae9d66cebed
Doyoun Kang <doyoun.kang@samsung.com>
2018-03-20 17:37:07 +0900

    e_service_region: check region->rotation value in _region_obj_geometry_update
    Change-Id: I1b660adc284334708f0cfea3ed70c65bf57b9fad

--- src/bin/services/e_service_region.c
+++ src/bin/services/e_service_region.c
@@ -45,6 +45,8 @@ _region_obj_geometry_update(E_Policy_Region *region)
    E_Policy_Angle_Map r;
 
    r = region->rotation;
+   if ((r < E_POLICY_ANGLE_MAP_0 ) || (r > E_POLICY_ANGLE_MAP_NUM))
+     return;
 
    INF("Update Geometry: rotation %d x %d y %d w %d h %d",
        e_policy_angle_get(r), region->geom[r].x, region->geom[r].y, region->geom[r].w, region->geom[r].h);
