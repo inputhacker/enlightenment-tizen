fcd6e1304b530daf579174876d069dd90f3930bf
Seunghun Lee <shiin.lee@samsung.com>
2018-12-12 11:21:45 +0900

    video: Added 'src/bin/video' directory to CFLAGS and remove relative path for video from "e.h"
    this fixes build break of modules.
    
    Change-Id: I3250a14b88e22483d1d5a13a71068eebed0f939e

--- src/modules/Makefile.mk
+++ src/modules/Makefile.mk
@@ -3,7 +3,9 @@ MOD_LDFLAGS = -module -avoid-version
 MOD_CPPFLAGS = -I. -DE_LOGGING=1 \
 -I$(top_srcdir) \
 -I$(top_srcdir)/src/bin \
+-I$(top_srcdir)/src/bin/video \
 -I$(top_builddir)/src/bin \
+-I$(top_builddir)/src/bin/video \
 -I$(top_srcdir)/src/modules \
 @e_cflags@ \
 @WAYLAND_CFLAGS@ \
