693b5d69e45f992f0ebb986bbe84614105375c25
Junseok, Kim <juns.kim@samsung.com>
2019-04-25 18:42:16 +0900

    e_policy_appinfo: switch visibility of appinfo functions for module
    Change-Id: Iad1e8d291b08b13b3d6ffc6ea288a97a24bf8231

--- src/bin/e_policy_appinfo.h
+++ src/bin/e_policy_appinfo.h
@@ -6,7 +6,7 @@ EINTERN Eina_Bool         e_policy_appinfo_pid_set(E_Policy_Appinfo *epai, pid_t
 EINTERN Eina_Bool         e_policy_appinfo_appid_set(E_Policy_Appinfo *epai, const char *appid);
 EINTERN Eina_Bool         e_policy_appinfo_base_output_resolution_get(E_Policy_Appinfo *epai, int *width, int *height);
 
-EINTERN E_Policy_Appinfo *e_policy_appinfo_find_with_pid(pid_t pid);
-EINTERN Eina_Bool         e_policy_appinfo_base_output_resolution_set(E_Policy_Appinfo *epai, int width, int height);
+E_API E_Policy_Appinfo *e_policy_appinfo_find_with_pid(pid_t pid);
+E_API Eina_Bool         e_policy_appinfo_base_output_resolution_set(E_Policy_Appinfo *epai, int width, int height);
 
 
