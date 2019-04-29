b19a45bb96f6112a150bfeaab1e8d03c5e00e392
Seunghun Lee <shiin.lee@samsung.com>
2019-04-29 17:29:52 +0900

    video: Correct name of variable for E_Video_External as proper abbreviation.
    Change-Id: Iaf82ce80eac680cb7ecfc4c8994b9b9b35be24ed

--- src/bin/video/iface/e_video_external.c
+++ src/bin/video/iface/e_video_external.c
@@ -11,12 +11,12 @@ struct _E_Video_External
 static void
 _e_video_external_iface_destroy(E_Video_Comp_Iface *iface)
 {
-   E_Video_External *evs;
+   E_Video_External *eve;
 
-   evs = container_of(iface, E_Video_External, base);
-   e_client_video_comp_redirect_unset(evs->ecv);
+   eve = container_of(iface, E_Video_External, base);
+   e_client_video_comp_redirect_unset(eve->ecv);
 
-   free(evs);
+   free(eve);
 }
 
 static void
@@ -28,12 +28,12 @@ _e_video_external_init(E_Client_Video *ecv)
 EINTERN E_Video_Comp_Iface *
 e_video_external_iface_create(E_Client_Video *ecv)
 {
-   E_Video_External *evs;
+   E_Video_External *eve;
 
    INF("Intializing External Compositing mode");
 
-   evs = E_NEW(E_Video_External, 1);
-   if (!evs)
+   eve = E_NEW(E_Video_External, 1);
+   if (!eve)
      {
         ERR("Failed to create E_Video_External");
         return NULL;
@@ -41,8 +41,8 @@ e_video_external_iface_create(E_Client_Video *ecv)
 
    _e_video_external_init(ecv);
 
-   evs->ecv = ecv;
-   evs->base.destroy = _e_video_external_iface_destroy;
+   eve->ecv = ecv;
+   eve->base.destroy = _e_video_external_iface_destroy;
 
-   return &evs->base;
+   return &eve->base;
 }
