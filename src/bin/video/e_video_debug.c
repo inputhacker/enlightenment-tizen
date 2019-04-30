7e51ff4eef8df4d8058efc6506deacd60dfedc6a
Seunghun Lee <shiin.lee@samsung.com>
2019-04-30 10:03:13 +0900

    video: Added e_video_debug_screen_punch_set()/unset() functions.
    This API will make full-sized transparent object overlapping canvas of
    e_comp to show video plane.
    
    Change-Id: I05976b1465ca486951e6c756d1ec7541fa3b20ad

--- src/bin/video/e_video_debug.c
+++ src/bin/video/e_video_debug.c
@@ -2,6 +2,7 @@
 
 static Eina_Bool video_to_primary = EINA_FALSE;
 static Eina_Bool video_punch = EINA_FALSE;
+static Evas_Object *punch_obj = NULL;
 
 E_API Eina_Bool
 e_video_debug_display_primary_plane_value_get(void)
@@ -25,4 +26,34 @@ EINTERN void
 e_video_debug_punch_set(Eina_Bool set)
 {
    video_punch = set;
+}
+
+EINTERN void
+e_video_debug_screen_punch_set(int x, int y, int w, int h, int a, int r, int g, int b)
+{
+   if (!punch_obj)
+     {
+        punch_obj = evas_object_rectangle_add(e_comp->evas);
+        evas_object_layer_set(punch_obj, EVAS_LAYER_MAX);
+        evas_object_render_op_set(punch_obj, EVAS_RENDER_COPY);
+     }
+
+   evas_object_color_set(punch_obj, r, g, b, a);
+
+   if (w == 0 || h == 0)
+     evas_output_size_get(e_comp->evas, &w, &h);
+
+   evas_object_move(punch_obj, x, y);
+   evas_object_resize(punch_obj, w, h);
+   evas_object_show(punch_obj);
+}
+
+EINTERN void
+e_video_debug_screen_punch_unset(void)
+{
+   if (!punch_obj)
+     return;
+
+   evas_object_del(punch_obj);
+   punch_obj = NULL;
 }
