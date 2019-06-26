6e98aeb0bf98e2b026d31a77817ddeb28be882dd
SooChan Lim <sc1.lim@samsung.com>
2019-06-26 19:27:30 +0900

    e_client: Do not cacluate the base_output_resolution transform of the parent_ec
    Do not calcuate the base_output_resolution transform of the parent_ec
    when the ec is a subsurface of the parent_ec and when the ec calculate the transform matrix.
    
    Change-Id: I2073621a15ad70424503c6b537bd5bf5b8239324

--- src/bin/e_client.c
+++ src/bin/e_client.c
@@ -3621,6 +3621,7 @@ _e_client_transform_core_check_change(E_Client *ec)
    if (ec->comp_data)
      {
         E_Comp_Wl_Client_Data *cdata = (E_Comp_Wl_Client_Data*)ec->comp_data;
+        E_Util_Transform_Matrix parent_matrix;;
 
         if (cdata->sub.data)
           {
@@ -3634,7 +3635,17 @@ _e_client_transform_core_check_change(E_Client *ec)
                                                            &parent->transform_core.result.matrix))
                     {
                        check = EINA_TRUE;
-                       ec->transform_core.parent.matrix = parent->transform_core.result.matrix;
+                       parent_matrix = parent->transform_core.result.matrix;
+                       if (parent->base_output_resolution.use)
+                         {
+                            e_util_transform_matrix_scale(&parent_matrix,
+                                                          (double)parent->base_output_resolution.w / (double)parent->desk->geom.w,
+                                                          (double)parent->base_output_resolution.h / (double)parent->desk->geom.h,
+                                                          1.0);
+                            ELOGF("POL_APPINFO", "Subsurface TRANSFORM... Remove the scale matrix of the parent ec(%p).", ec, parent);
+                         }
+
+                         ec->transform_core.parent.matrix = parent_matrix;
                     }
                }
              else if (ec->transform_core.parent.enable)
