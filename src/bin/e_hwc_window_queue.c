e4421447192b8b4753e7192a5bb47d01c24539b3
Changyeon Lee <cyeon.lee@samsung.com>
2019-07-23 21:34:30 +0900

    e_hwc_window_queue: add null check for hwc_window
    Change-Id: I7ff22a30292387dcf28b0c743a2824d076d671f6

--- src/bin/e_hwc_window_queue.c
+++ src/bin/e_hwc_window_queue.c
@@ -778,10 +778,14 @@ _e_hwc_window_queue_unset(E_Hwc_Window_Queue *queue)
    if (eina_list_count(queue->user_pending_set))
      {
         hwc_window = eina_list_nth(queue->user_pending_set, 0);
-        if (!_e_hwc_window_queue_prepare_set(queue, hwc_window))
+        if (hwc_window)
           {
-             EHWQERR("fail to queue_prepare_set for user_pending_set hwc_window:%p", NULL, queue->hwc, queue, hwc_window);
-             _e_hwc_window_queue_tqueue_release(queue->tqueue, hwc_window);
+             if (!_e_hwc_window_queue_prepare_set(queue, hwc_window))
+               {
+                  EHWQERR("fail to queue_prepare_set for user_pending_set hwc_window:%p",
+                          NULL, queue->hwc, queue, hwc_window);
+                  _e_hwc_window_queue_tqueue_release(queue->tqueue, hwc_window);
+               }
           }
      }
 
