9aa541f620716b0648f5e2ccadee2e34d0365b11
Junseok, Kim <juns.kim@samsung.com>
2019-08-23 16:12:10 +0900

    e_client: check argb and force_obscured while check fully_contain_by_above
    Change-Id: I3dafc53acbd9e8354ae97ad1215a54875d332aac

--- src/bin/e_client.c
+++ src/bin/e_client.c
@@ -819,6 +819,8 @@ _e_client_check_fully_contain_by_above(E_Client *ec, Eina_Bool check_layer)
             (!e_client_util_ignored_get(above)) &&
             (above->visible) &&
             (!above->iconic || e_policy_visibility_client_is_uniconic(above)) &&
+            (!above->argb) &&
+            (!above->visibility.force_obscured) &&
             (above->frame))
           {
              if (E_CONTAINS(above->x, above->y, above->w, above->h, x, y, w, h))
